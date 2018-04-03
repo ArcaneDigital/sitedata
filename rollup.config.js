97a9c57ce903c19f78b55a697a01c06a14915766
iamchristopher <chriswright@live.com>
2018-04-03 17:04:06 -0400

    Removed redundant entry point

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
-    input: 'index.js',
+    input: 'lib/sitedata.js',
     output: {
         file: 'dist.js',
         format: 'cjs'
