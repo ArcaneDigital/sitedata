c5e53672ec615fbfcd5519faf1ec6a3409a9a851
iamchristopher <chriswright@live.com>
2018-04-03 22:50:48 -0400

    Added tests to spy on network requests for index metric

--- test.js
+++ test.js
@@ -1,9 +1,67 @@
 const should = require('should');
+const proxy = require('proxyquire');
+const sinon = require('sinon');
+const qs = require('querystring');
 
-const Sitedata = require('./lib/sitedata');
+const requestSpy = sinon.spy(() => ({
+    body: JSON.stringify({
+        data: [],
+        links: {
+            next: ''
+        }
+    })
+}));
+const module = proxy('./lib/sitedata', {
+    got: requestSpy
+});
+const TEST_TOKEN = 'TEST_TOKEN';
+const TEST_URL = 'arcane.ws';
 
 describe('Module options', () => {
     it('should require API token', () => {
-        should.throws(() => new Sitedata({}));
+        should.throws(() => new module({}));
+    });
+});
+
+describe('Metrics', () => {
+    const sd = new module({ token: TEST_TOKEN });
+
+    describe("Index", async () => {
+        let result;
+
+        beforeEach(async () =>{
+            result = await sd.metric.index({ url: TEST_URL });
+        });
+
+        afterEach(async () =>{
+            requestSpy.resetHistory();
+        });
+
+        it("should make at least one request", () => {
+            should(requestSpy.called).be.true();
+        });
+
+        it("should get called with the correct params", () => {
+            const {
+                args: [[
+                    urlParam
+                ]]
+            } = requestSpy;
+            const [
+                uri,
+                queryParams
+            ] = urlParam.split('?');
+            const {
+                match,
+                page,
+                token,
+                url
+            } = qs.parse(queryParams);
+
+            should.equal(token, TEST_TOKEN);
+            should.equal(url, TEST_URL);
+            should.equal(page, '1');
+            should.equal(match, 'contains');
+        });
     });
 });
